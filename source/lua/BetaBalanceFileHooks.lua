4418d5b1a7060fe3b3340b0421fcd40be2bf7eb3
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2020-02-09 17:48:57 +0100

    Changing strings for focus into Blight

--- source/lua/BetaBalanceFileHooks.lua
+++ source/lua/BetaBalanceFileHooks.lua
@@ -1,1 +1,13 @@
 ModLoader.SetupFileHook("lua/NS2Utility.lua", "lua/BetaBalanceNS2Utility.lua", "post")
+
+if Locale then
+    local sntl_strings = {
+        ["FOCUS"] = "Blight",
+        ["FOCUS_TOOLTIP"] = "Upon a hit, parasites and indicates health",
+    }
+
+    local old_Locale_ResolveString = Locale.ResolveString
+    function Locale.ResolveString(text)
+        return sntl_strings[text] or old_Locale_ResolveString(text)
+    end
+end
