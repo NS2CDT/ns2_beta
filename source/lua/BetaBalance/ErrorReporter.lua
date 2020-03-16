bd4612b6fafc49c1801a5d46aba05afb3410591c
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2020-03-16 21:07:17 +0100

    Check for shine before loading error reporter

--- source/lua/BetaBalance/ErrorReporter.lua
+++ source/lua/BetaBalance/ErrorReporter.lua
@@ -1,6 +1,8 @@
 --[[
 	Mod error report handling.
 ]]
+if not Shine then return end -- Using some shine lib features
+
 local ErrorQueue = {}
 local Reported = {}
 
@@ -48,7 +50,7 @@ local function ReportErrors()
     TableEmpty( ErrorQueue )
 end
 
-if Server then
+if Server  then
     Shine.Hook.Add( "EndGame", "ReportQueuedErrors", ReportErrors )
 end
 
@@ -62,12 +64,10 @@ local function ReportError(Error)
     -- We cannot send error reports on disconnect/map change anymore due to HTTP requests being cancelled,
     -- thus we need to send errors as soon as possible after they occur to avoid them being lost.
     -- We debounce to ensure we catch a sequence of errors in a single request.
-    if Shine then
-        ErrorReportTimer = ErrorReportTimer or Shine.Timer.Simple( 1, function()
-            ErrorReportTimer = nil
-            ReportErrors()
-        end )
-        ErrorReportTimer:Debounce()
-    end
+    ErrorReportTimer = ErrorReportTimer or Shine.Timer.Simple( 1, function()
+        ErrorReportTimer = nil
+        ReportErrors()
+    end )
+    ErrorReportTimer:Debounce()
 end
 Event.Hook("ErrorCallback", ReportError)
