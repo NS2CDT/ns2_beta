0400281e6074c4b1f42244864a899d8146bfe80a
Darrell Gentry <dgentry.email@protonmail.com>
2020-04-03 18:29:01 -0700

    Update README and version for Blight fixes/tweaks

--- source/lua/BetaBalanceShared.lua
+++ source/lua/BetaBalanceShared.lua
@@ -1,4 +1,4 @@
-kBBMVersion = 1202004031 -- 1 year month day versionofday
+kBBMVersion = 1202004032 -- 1 year month day versionofday
 
 Script.Load("lua/BalanceDebugging.lua")
 Script.Load("lua/BetaBalance/ErrorReporter.lua")
