8fb679e0520a34ed5c190442d0f4047cf9c1c56b
Darrell Gentry <dgentry.email@protonmail.com>
2020-04-14 00:08:25 -0700

    Make web cloaking client side.
    - Alien players will have the "cloaked" shader applied when the web should be invisible to marines.
    - Removed CloakableMixin from gorge web.
    - Remove ScanForNearbyEnemies timed callback. (Related to cloakablemixin)

--- source/lua/Weapons/Alien/Web.lua
+++ source/lua/Weapons/Alien/Web.lua
@@ -25,7 +25,6 @@ Script.Load("lua/ClogFallMixin.lua")
 Script.Load("lua/Mixins/BaseModelMixin.lua")
 Script.Load("lua/Mixins/ModelMixin.lua")
 Script.Load("lua/EffectsMixin.lua")
-Script.Load("lua/CloakableMixin.lua")
 
 class 'Web' (Entity)
 
@@ -33,8 +32,8 @@ Web.kMapName = "web"
 
 Web.kRootModelName = PrecacheAsset("models/alien/gorge/web_helper.model")
 Web.kModelName = PrecacheAsset("models/alien/gorge/web.model")
-local kAnimationGraph = PrecacheAsset("models/alien/gorge/web.animation_graph")
 
+local kAnimationGraph = PrecacheAsset("models/alien/gorge/web.animation_graph")
 local kEnemyDetectInterval = 0.2
 
 local networkVars =
@@ -50,12 +49,12 @@ Web.kDistortionIntensity = 0.0625
 Web.ChargeScaleAdditive = 0.47 -- Percent to "thicken" webs by per charge.
 
 local kWebDistortMaterial = PrecacheAsset("models/alien/gorge/web_distort.material")
+local kWebCloakedMaterial = PrecacheAsset("cinematics/vfx_materials/cloaked.material")
 
 AddMixinNetworkVars(TechMixin, networkVars)
 AddMixinNetworkVars(BaseModelMixin, networkVars)
 AddMixinNetworkVars(ModelMixin, networkVars)
 AddMixinNetworkVars(TeamMixin, networkVars)
-AddMixinNetworkVars(CloakableMixin, networkVars)
 AddMixinNetworkVars(LiveMixin, networkVars)
 AddMixinNetworkVars(LOSMixin, networkVars)
 
@@ -120,7 +119,6 @@ function Web:OnCreate()
     InitMixin(self, TeamMixin)
     InitMixin(self, LiveMixin)
     InitMixin(self, EntityChangeMixin)
-    InitMixin(self, CloakableMixin)
     InitMixin(self, LOSMixin)
 
     InitMixin(self, ClogFallMixin)
@@ -181,32 +179,12 @@ local function GetAreEnemiesInRange(self)
 
 end
 
-local function ScanForNearbyEnemies(self)
-    
-    PROFILE("Web:ScanForNearbyEnemies")
-    
-    if GetAreEnemiesInRange(self) then
-    
-        self:TriggerUncloak()
-    
-    end
-    
-    return (self:GetIsAlive()) -- stop callback when dead.
-    
-end
-
 function Web:OnInitialized()
 
     self:SetModel(Web.kModelName, kAnimationGraph)
     
     self:SetPhysicsType(PhysicsType.Kinematic)
     self:SetPhysicsGroup(PhysicsGroup.WebsGroup)
-    
-    if Server then
-        
-        self:AddTimedCallback(ScanForNearbyEnemies, kEnemyDetectInterval)
-        
-    end
   
 end
 
@@ -308,10 +286,48 @@ if Client then
         
         -- TODO these values should be baked into the shader, but they're exposed and set here every
         -- frame so that balance team can tweak values as they please.
-        if self.distortMaterial then
-            self.distortMaterial:SetParameter("noVisDist", Web.kZeroVisDistance)
-            self.distortMaterial:SetParameter("fullVisDist", Web.kFullVisDistance)
-            self.distortMaterial:SetParameter("distortionIntensity", Web.kDistortionIntensity)
+
+        local player = Client.GetLocalPlayer()
+        local model = self:GetRenderModel()
+
+        if player and model then
+            local isFriendly = GetAreFriends(self, player)
+            local distance = (self:GetOrigin() - player:GetOrigin()):GetLength()
+            local cloakAmount = Clamp((distance - Web.kZeroVisDistance) / (Web.kZeroVisDistance - Web.kFullVisDistance), 0, 1)
+
+            if isFriendly then
+                if not self.cloakedMaterial then
+                    self.cloakedMaterial = AddMaterial(model, kWebCloakedMaterial)
+                end
+
+                if self.distortMaterial then
+                    RemoveMaterial(model, self.distortMaterial)
+                    self.distortMaterial = nil
+                end
+            else
+                if not self.distortMaterial then
+                    self.distortMaterial = AddMaterial(model, kWebDistortMaterial)
+                end
+
+                if self.cloakedMaterial then
+                    RemoveMaterial(model, self.cloakedMaterial)
+                    self.cloakedMaterial = nil
+                end
+            end
+
+            if self.cloakedMaterial then
+
+                self:SetOpacity(1 - cloakAmount, "cloak")
+                self.cloakedMaterial:SetParameter("cloakAmount", Clamp(cloakAmount, 0, 0.3))
+            end
+
+            if self.distortMaterial then
+                self:SetOpacity(1 - cloakAmount, "cloak")
+                self.distortMaterial:SetParameter("noVisDist", Web.kZeroVisDistance)
+                self.distortMaterial:SetParameter("fullVisDist", Web.kFullVisDistance)
+                self.distortMaterial:SetParameter("distortionIntensity", Web.kDistortionIntensity)
+            end
+
         end
         
     end
